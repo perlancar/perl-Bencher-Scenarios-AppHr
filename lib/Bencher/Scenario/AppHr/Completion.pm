c0207c19e0b7bba1ac224c507010c2c1d15d2cb7
perlancar (@pc-office) <perlancar@gmail.com>
2016-03-10 14:41:36 +0700

    [Bugfix] Fix scenario specification (specifying version for module) #2

--- lib/Bencher/Scenario/AppHr/Completion.pm
+++ lib/Bencher/Scenario/AppHr/Completion.pm
@@ -12,7 +12,7 @@ use Bencher::ScenarioUtil::Completion qw(make_completion_participant);
 our $scenario = {
     summary => 'Benchmark completion response time, to monitor regression',
     modules => {
-        'App::hr' => 0,
+        'App::hr' => {version=>0},
     },
     participants => [
         make_completion_participant(
